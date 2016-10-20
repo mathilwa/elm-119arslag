0bece41b879f1c7c93d2a5b72b1202c2dace884b
mathilwa <G4s32a15>
2016-10-20 14:38:20 +0200

    Fått opp mer info

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -41,15 +41,41 @@ view model =
         , div [] [ text ("Torunn og Trond fyller 119 år.") ]
         ]
       ]
-      , div [ class "information pure-g" ][
-          div [ class "pure-u-1-2" ][
-            div [ class "l-box" ][
-              h3 [ class "information-head" ][ text ("Hei alle kjære venner og familie av mamma og pappa/Torunn og Trond!")]
-              , p [] [ text ("Lørdag 3. juni 2017 vil vi gjerne få invitere dere til å bli med å feire at mamma og pappa fyller 60 år. Hold av datoen, så håper vi dere har tid, lyst og mulighet til å være med å gi disse fantastiske menneskene en bursdag de sent vil glemme!
-                              Alle er invitert med kjæreste/ektefelle/samboer, så spre gjerne ordet videre!") ]
-            ]
+      , div [ class "l-content" ] [
+          div [ class "information pure-g" ][
+              div [ class "pure-u-1" ][
+                div [ class "l-box" ][
+                  h3 [ class "information-head" ][ text ("Hei alle kjære venner og familie av mamma og pappa/Torunn og Trond!")]
+                  , p [] [ text ("Lørdag 3. juni 2017 vil vi gjerne få invitere dere til å bli med å feire at mamma og pappa fyller 60 år. Hold av datoen, så håper vi dere har tid, lyst og mulighet til å være med å gi disse fantastiske menneskene en bursdag de sent vil glemme!
+                                  Alle er invitert med kjæreste/ektefelle/samboer, så spre gjerne ordet videre!") ]
+                ]
+              ]
+          ]
+          , div [ class "information pure-g" ][
+                    div [ class "pure-u-1-3" ][
+                      div [ class "l-box" ][
+                        h3 [ class "information-head" ][ text ("Påmelding")]
+                        , p [] [ text ("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamco laboris nisi ut aliquip ex ea commodo
+                                                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.") ]
+                      ]
+                    ]
+                , div [ class "pure-u-1-3" ][
+                    div [ class "l-box" ][
+                       h3 [ class "information-head" ][ text ("Festen")]
+                       , p [] [ text ("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamco laboris nisi ut aliquip ex ea commodo
+                                                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.") ]
+                    ]
+                ]
+                , div [ class "pure-u-1-3" ][
+                    div [ class "l-box" ][
+                      h3 [ class "information-head" ][ text ("Overnatting")]
+                      , p [] [ text ("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamco laboris nisi ut aliquip ex ea commodo
+                                                          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.") ]
+                    ]
+                 ]
           ]
       ]
+
   ]
 
 
